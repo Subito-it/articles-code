14099e82c3fad8905be80ec324a27d5eeeeb98bd
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2025-11-15 16:59:46 +0100

    feat: add a11y example

--- visual-regression-example/playwright.config.ts
+++ visual-regression-example/playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-    outputDir: './test/visual/output/results',
-    testDir: './test/visual',
+    outputDir: './test/output/results',
+    testDir: './test',
     retries: 2,
     use: {
         screenshot: 'only-on-failure',
